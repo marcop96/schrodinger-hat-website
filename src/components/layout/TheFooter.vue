e569f74eaee6da1ab21fad4c86716f1dd560d5c1
“Marco <marco.pianaroli96@gmail.com>
2024-01-04 18:04:00 +0100

    fix:LogoAnimated width

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -8,7 +8,7 @@ import LogoAnimated from '@/components/buttons/LogoAnimated.vue'
     <SeparatorTripleLine class="absolute bottom-16" />
     <div class="container flex justify-center pt-10 text-left">
       <div class="flex justify-between items-center w-full h-full px-2 mx-auto">
-        <LogoAnimated />
+        <LogoAnimated class="w-[275px]" />
         <div>
           <a href='/privacy-and-permissions'>Privacy Permissions</a>
           <span class="m-4">|</span>
